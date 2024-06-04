4bd28f3f73d05d78c219e64ce62265ef281845dc
Mavenark <tech@mavenark.co.in>
2024-06-04 15:22:01 +0530

    disk folder

--- index.js
+++ index.js
@@ -831,3 +831,11 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./dist/index.html"));
+});
