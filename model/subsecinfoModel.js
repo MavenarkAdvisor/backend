408462ec0be756c5d0ec32c0e37798c6b1637412
Mavenark <tech@mavenark.co.in>
2024-07-18 11:23:40 +0530

    update subsecinfo

--- model/subsecinfoModel.js
+++ model/subsecinfoModel.js
@@ -1,29 +1,44 @@
 const mongoose = require("mongoose");
 
-const secDetailSchema = new mongoose.Schema(
+const subsecinfoSchema = new mongoose.Schema(
   {
-    SecurityCode: String,
+    SubSecCode: String,
+    ValuationDate: Date,
+    SystemDate: Date,
+    SecCode: String,
     ISIN: String,
-    SecurityDescription: String,
-    AssetType: String,
-    IssuanceDate: Number,
-    MaturityDate: Number,
+    SecurityName: String,
+    YTM: Number,
+    FaceValue: Number,
     CouponRate: Number,
     CouponType: String,
-    Frequency: String,
-    Maturity: String,
-    Seniority: String,
-    Security: String,
-    IssuerShortName: String,
-    IssuerFullName: String,
-    Sector: String,
-    Exchange: String,
-    RDDays: Number,
-    RDType: String,
+    LIPDate: Date,
+    NIPDate: Date,
+    RecordDate: Date,
+    NIPDateForSettlement: Date,
+    LIPDateForSettlement: Date,
+    DCB: Number,
+    IntAccPerDay: Number,
+    DirtyPriceForSettlement: Number,
+    IntAccPerDayForSettlement: Number,
+    CleanPriceforSettlement: Number,
+    Priceper100: Number,
+    FaceValueForValuation: Number,
+    MaturityDate: Date,
+    LipDateForValuation: Date,
+    DirtyPriceForValuation: Number,
+    PrincipalRedemptionSinceLIP: Number,
+    IntAccPerDayForValuation: Number,
+    CleanPriceforValuation: Number,
+    PRDPrincipal: Number,
+    PRDInterest: Number,
+    CleanPriceForPRDUnits: Number,
+    MacaulayDuration: Number,
+    ModifiedDuration: Number,
   },
   { timestamps: true, require: true }
 );
 
-const secDetail = mongoose.model("secDetail", secDetailSchema);
+const Subsecinfo = mongoose.model("subsecinfo", subsecinfoSchema);
 
-module.exports = secDetail;
+module.exports = Subsecinfo;
